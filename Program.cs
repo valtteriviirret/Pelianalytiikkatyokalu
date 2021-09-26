77adb183a39e4eae89fefea3bae930809749741b
viirretvaltteri@gmail.com <viirretvaltteri@gmail.com>
2021-09-27 02:50:25 +0300

    average play time

--- Program.cs
+++ Program.cs
@@ -50,6 +50,7 @@ namespace TietokantaTesti
                 {
                     case "keskiostos": tool.AverageBuy(); break;
                     case "mediaaniostos": tool.MedianBuy(); break;
+                    case "keskipeliaika": tool.AveragePlaytime(); break;
                     default: tool.AssingQuery(); break;
                 }
 
@@ -169,6 +170,38 @@ namespace TietokantaTesti
             mid = size / 2;
             median = (size % 2 != 0) ? nums[mid] : (nums[mid] + nums[mid - 1]) / 2;
             Console.WriteLine(Math.Round(median, 4));
+        }
+
+        public void AveragePlaytime()
+        {
+            MySqlCommand cmd = new MySqlCommand("select alkuaika, loppuaika from Pelisessio", cnn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<DateTime> nums = new List<DateTime>();
+            List<DateTime> starts = new List<DateTime>();
+            List<DateTime> ends = new List<DateTime>();
+            List<TimeSpan> values = new List<TimeSpan>();
+            TimeSpan alltime = new TimeSpan(0, 0, 0); 
+
+            while(reader.Read())
+                for(int i = 0; i < reader.FieldCount; i++)
+                    nums.Add(reader.GetDateTime(i));
+            
+            reader.Close();
+            int n = 0;
+            for(int i = 0; i < nums.Count; i++)
+            {
+                if(n % 2 != 0)
+                    starts.Add(nums[i]);
+                else
+                    ends.Add(nums[i]);
+                n++;
+            }
+            for(int i = 0; i < starts.Count; i++)
+                values.Add(starts[i].Subtract(ends[i]));
+            
+            for(int i = 0; i < values.Count; i++)
+                alltime += values[i];
+            Console.WriteLine(alltime / values.Count);
         }
     }
 } 
